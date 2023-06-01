7f1bb4749315c0ac0af8d1a8b68b3e4f3e53779f
Wes Henderson <whendersonii@gmail.com>
2023-06-01 18:49:23 -0500

    Starting to add schema rules.

--- src/json-resume-cli/resume_schema.py
+++ src/json-resume-cli/resume_schema.py
@@ -2,25 +2,25 @@
 import schema
 
 SCHEMA = schema.Schema({
-    "meta": {
-        "theme": str,
+    schema.Optional("meta"): {
+        schema.Optional("theme"): str,
     },
-    "basics": {
-        "name": str,
-        "label": str,
-        "image": str,
-        "email": str,
-        "phone": str,
-        "url": str,
-        "summary": str,
-        "location": {
-            "city": str,
-            "countryCode": str
+    schema.Optional("basics"): {
+        schema.Optional("name"): str,
+        schema.Optional("label"): str,
+        schema.Optional("image"): str,
+        schema.Optional("email"): str,
+        schema.Optional("phone"): str,
+        schema.Optional("url"): str,
+        schema.Optional("summary"): str,
+        schema.Optional("location"): {
+            schema.Optional("city"): str,
+            schema.Optional("countryCode"): str
         },
         schema.Optional("profiles"): list,
     },
-    schema.Optional("Skills"): list,
+    schema.Optional("skills"): list,
     schema.Optional("work"): list,
-    schema.Optional("Certifications"): list,
+    schema.Optional("certificates"): list,
     schema.Optional("education"): list,
     }, ignore_extra_keys=True)
